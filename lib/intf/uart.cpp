0b8c83476f711bb0c97af3f76e5be9c4ea725342
Jonathan Ludwig <jr.ludwig@gmail.com>
2018-12-03 20:31:14 +0000

    Created PlatformIO project for firmware

---
+++ lib/intf/uart.cpp
@@ -0,0 +1,7 @@
+#include "uart.h"
+
+
+SerialInterface *uart__get_serial_interface(size_t uart_number)
+{
+    return NULL;
+}
--- src/fw/gps_transaction_recorder/gps_transaction_serializer.cpp
+++ src/fw/gps_transaction_recorder/gps_transaction_serializer.cpp
@@ -1,4 +1,4 @@
-#include "uart_transaction_serializer.h"
+#include "gps_transaction_serializer.h"
 
 
 bool GpsTransactionSerializer::serialize_nmea_sentence(uint8_t *transaction_stream,
@@ -7,7 +7,7 @@ bool GpsTransactionSerializer::serialize_nmea_sentence(uint8_t *transaction_stre
                                 uint64_t transaction_time,
                                 const char *sentence)
 {
-
+    return false;
 }
 
 bool GpsTransactionSerializer::serialize_pps_pulse(uint8_t *transaction_stream,
@@ -15,7 +15,7 @@ bool GpsTransactionSerializer::serialize_pps_pulse(uint8_t *transaction_stream,
                                 size_t &bytes_written_to_stream,
                                 uint64_t transaction_time)
 {
-
+    return false;
 }
 
 bool GpsTransactionSerializer::desrialize_uart_transaction(uint8_t *transaction_stream,
@@ -23,7 +23,7 @@ bool GpsTransactionSerializer::desrialize_uart_transaction(uint8_t *transaction_
                                     size_t &bytes_read_from_stream,
                                     GpsTransaction &deserialized_transaction)
 {
-
+    return false;
 }
 
 
