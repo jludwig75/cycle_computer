c2ec7f9eebd3e6872b77986bc56606575e3895c7
Jonathan Ludwig <jr.ludwig@gmail.com>
2018-12-07 22:50:26 +0000

    Finished VFSFileImpl implementation

--- src/sim/SPIFFS_sim/vfs.cpp
+++ src/sim/SPIFFS_sim/vfs.cpp
@@ -53,6 +53,36 @@ bool        VFSImpl::rmdir(const char *path)
 }
 
 
+static bool startsWith(const String &str, const String & start)
+{
+    if (start.length() == 0)
+    {
+        return true;
+    }
+
+    if (start.length() > str.length())
+    {
+        return false;
+    }
+
+    return start.substr(0, start.length()) == start;
+}
+
+static bool endsWith(const String &str, const String & start)
+{
+    if (start.length() == 0)
+    {
+        return true;
+    }
+
+    if (start.length() > str.length())
+    {
+        return false;
+    }
+
+    return str.substr(str.length() - start.length(), start.length()) == start;
+}
+
 
 VFSFileImpl::VFSFileImpl(VFSImpl* fs, const char* path, const char* mode) :
     _fs(fs),
@@ -140,59 +170,124 @@ size_t      VFSFileImpl::read(uint8_t* buf, size_t size)
 
 void        VFSFileImpl::flush()
 {
+    if(_isDirectory || !_f) {
+        return;
+    }
+    fflush(_f);
 }
 
 bool        VFSFileImpl::seek(uint32_t pos, SeekMode mode)
 {
-    return false;
+    if(_isDirectory || !_f) {
+        return false;
+    }
+    auto rc = fseek(_f, pos, mode);
+    return rc == 0;
 }
 
 size_t      VFSFileImpl::position() const
 {
-    return 0;
+    if(_isDirectory || !_f) {
+        return 0;
+    }
+    return ftell(_f);
 }
 
 size_t      VFSFileImpl::size() const
 {
-    return 0;
+    if(_isDirectory || !_f) {
+        return 0;
+    }
+    if (_written) {
+        _getStat();
+    }
+    return _stat.st_size;
 }
 
 void        VFSFileImpl::close()
 {
-    if (_f)
+    if(_path)
+    {
+        free(_path);
+        _path = NULL;
+    }
+    if(_isDirectory && _d)
+    {
+        closedir(_d);
+        _d = NULL;
+        _isDirectory = false;
+    }
+    else if(_f)
     {
         fclose(_f);
         _f = NULL;
     }
-
 }
 
 const char* VFSFileImpl::name() const
 {
-    return NULL;
+    return (const char*) _path;
 }
 
 time_t VFSFileImpl::getLastWrite()
 {
-    return 0;
+    _getStat() ;
+    return _stat.st_mtime;
+}
+
+void VFSFileImpl::_getStat() const
+{
+    if(!_path) {
+        return;
+    }
+    char * temp = (char *)malloc(strlen(_path)+strlen(_fs->_mountpoint)+1);
+    if(!temp) {
+        return;
+    }
+    sprintf(temp,"%s%s", _fs->_mountpoint, _path);
+    if(!stat(temp, &_stat)) {
+        _written = false;
+    }
+    free(temp);
 }
 
 boolean     VFSFileImpl::isDirectory(void)
 {
-    return false;
+    return _isDirectory;
 }
 
 FileImplPtr VFSFileImpl::openNextFile(const char* mode)
 {
-    return FileImplPtr();
+    if(!_isDirectory || !_d) {
+        return FileImplPtr();
+    }
+    struct dirent *file = readdir(_d);
+    if(file == NULL) {
+        return FileImplPtr();
+    }
+    if(file->d_type != DT_REG && file->d_type != DT_DIR) {
+        return openNextFile(mode);
+    }
+    String fname = String(file->d_name);
+    String name = String(_path);
+    if(!startsWith(fname, "/") && !endsWith(name, "/")) {
+        name += "/";
+    }
+    name += fname;
+
+    return std::make_shared<VFSFileImpl>(_fs, name.c_str(), mode);
 }
 
 void        VFSFileImpl::rewindDirectory(void)
 {
+    if(!_isDirectory || !_d) {
+        return;
+    }
+    rewinddir(_d);
 }
 
 VFSFileImpl::operator    bool()
 {
-    return false;
+    return (_isDirectory && _d != NULL) || _f != NULL;
 }
 
