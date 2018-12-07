e90d038c8baa942ba12b1ded8a429c6a4c7438a5
Jonathan Ludwig <jr.ludwig@gmail.com>
2018-12-07 23:03:45 +0000

    Fixed compiler warning

--- src/fw/transaction_logger/transaction_log_entry.cpp
+++ src/fw/transaction_logger/transaction_log_entry.cpp
@@ -5,15 +5,15 @@
 
 
 transaction_log_entry::transaction_log_entry() :
-    time_stamp(0),
     id(0),
+    time_stamp(0),
     transaction_owner(0)
 {
 }
 
 transaction_log_entry::transaction_log_entry(uint64_t time_stamp, uint8_t transaction_owner) :
-    time_stamp(time_stamp),
     id(LOGGER_TRANSACTION_ID),
+    time_stamp(time_stamp),
     transaction_owner(transaction_owner)
 {
 }
