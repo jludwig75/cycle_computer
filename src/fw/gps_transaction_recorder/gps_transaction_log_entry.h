bfdf088cb7fba6f77a27428e6727660586038aa4
Jonathan Ludwig <jr.ludwig@gmail.com>
2018-12-07 23:01:58 +0000

    Made log entry structures packed and removed vtable

--- src/fw/gps_transaction_recorder/gps_transaction_log_entry.h
+++ src/fw/gps_transaction_recorder/gps_transaction_log_entry.h
@@ -3,20 +3,20 @@
 #include "transaction_logger/transaction_log_entry.h"
 
 
-struct gps_transaction_log_entry : public transaction_log_entry
+struct __attribute__((__packed__)) gps_transaction_log_entry : public transaction_log_entry
 {
     gps_transaction_log_entry(uint64_t time_stamp, uint8_t transaction_type);
     uint8_t transaction_type;
 };
 
-struct nmea_transaction_log_entry : public gps_transaction_log_entry
+struct __attribute__((__packed__)) nmea_transaction_log_entry : public gps_transaction_log_entry
 {
     nmea_transaction_log_entry(uint64_t time_stamp, uint8_t sentence_length);
     size_t entry_bytes() const;
     uint8_t sentence_length;
 };
 
-struct pps_transaction_log_entry : public gps_transaction_log_entry
+struct __attribute__((__packed__)) pps_transaction_log_entry : public gps_transaction_log_entry
 {
     pps_transaction_log_entry(uint64_t time_stamp);
     size_t entry_bytes() const;
--- src/fw/transaction_logger/transaction_log_entry.h
+++ src/fw/transaction_logger/transaction_log_entry.h
@@ -7,11 +7,10 @@
 #define GPS_TRANSACTION_OWNER_ID  1
 
 
-struct transaction_log_entry
+struct __attribute__((__packed__)) transaction_log_entry
 {
     transaction_log_entry();
     transaction_log_entry(uint64_t time_stamp, uint8_t transaction_owner);
-    virtual size_t entry_bytes() const = 0;
     uint16_t id;
     uint64_t time_stamp;
     uint8_t transaction_owner;
